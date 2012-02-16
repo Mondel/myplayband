43652dbcade765637dc89d049be0e7e68b745021
nicolas <nico.racing.99@gmail.com>
2012-02-16 17:06:59 -0200

    configuro los bundles para iniciar portada en SitioBundle

--- src/MyPlayBand/MusicaBundle/Controller/DefaultController.php
+++ src/MyPlayBand/MusicaBundle/Controller/DefaultController.php
@@ -8,8 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('MyPlayBandMusicaBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('MyPlayBandMusicaBundle:Default:index.html.twig');
     }
 }
